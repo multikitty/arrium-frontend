bccbc72585ef4b2b801a63112653c4fbb6060cd9
multikitty <multikitty422@gmail.com>
2023-01-30 13:02:24 +0530

    WEB-595 Error messages fixed (#219)

--- src/validation/signup/amazonFlex.ts
+++ src/validation/signup/amazonFlex.ts
@@ -3,9 +3,9 @@ import { yupResolver } from "@hookform/resolvers/yup"
 
 const amazonFlexSchema = Yup.object().shape({
   userName: Yup.string()
-    .email("The User Name field must be an email")
-    .required("The User Name field must not be empty"),
-  password: Yup.string().required("The Password field must not be empty"),
+    .email("Please enter valid amazon flex username.")
+    .required("Please enter valid amazon flex username."),
+  password: Yup.string().required("Please enter valid password"),
   confirmPassword: Yup.string()
     .required("The Confirm Password field must not be empty")
     .oneOf([Yup.ref("password"), null], "Passwords do not match"),
