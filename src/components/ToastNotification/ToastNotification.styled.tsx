9507f4d2d4a91368d5a6fee1058b4dd9da084bca
multikitty <multikitty422@gmail.com>
2023-05-15 12:46:20 +0530

    success-message-container-style-issue

--- src/components/ToastNotification/ToastNotification.styled.tsx
+++ src/components/ToastNotification/ToastNotification.styled.tsx
@@ -1,27 +1,28 @@
 import styled from "styled-components";
 export const ToastContainerWrapper = styled.div`
   .Toastify__toast-container {
+    width:auto;
     z-index: 9999;
   }
   .Toastify__toast {
     font-family: normal;
-    width:auto;
+    white-space: pre;
     font-size: 14px;
     color:#0A0A0A;
   }
   .Toastify__toast--success {
     background-color: #FFFFFF;
-    border:2px solid #2DB560;
+    border:1px solid #2DB560;
     border-radius: 8px;
   }
   .Toastify__toast--warning {
     background-color: #FFFFFF;
-    border:2px solid #FAB11E;
+    border:1px solid #FAB11E;
     border-radius: 8px;
   }
   .Toastify__toast--error {
     background-color: #FFFFFF;
-    border:2px solid #FA6464;
+    border:1px solid #FA6464;
     border-radius: 8px;
   }
   .Toastify__close-button {
